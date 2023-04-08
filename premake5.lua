96f36bb062c33631ead9a1fbb9ad38e31badc209
AcquaPorosa <dubrovskyyluca@gmail.com>
2023-04-08 13:26:50 +0200

    fix premake

--- premake5.lua
+++ premake5.lua
@@ -1,48 +1,57 @@
-project "GLFW"
+project "Glfw"
+	targetname "glfw"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
 
-   targetdir "%{wks.location}/bin/%{prj.name}/%{cfg.buildcfg}"
-   objdir "%{wks.location}/bin/%{prj.name}/%{cfg.buildcfg}/obj"
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
+	"include/GLFW/glfw3.h",
+    "include/GLFW/glfw3native.h",
+    "src/internal.h",
+    "src/platform.h",
+    "src/mappings.h",
+    "src/context.c",
+    "src/init.c",
+    "src/input.c",
+    "src/monitor.c",
+    "src/platform.c",
+    "src/vulkan.c",
+    "src/window.c",
+    "src/egl_context.c",
+    "src/osmesa_context.c",
+    "src/null_platform.h",
+    "src/null_joystick.h",
+    "src/null_init.c",
+    "src/null_monitor.c",
+    "src/null_window.c",
+    "src/null_joystick.c",
 	}
 
 	filter "system:linux"
 		pic "On"
 
 		systemversion "latest"
+		staticruntime "On"
 		
 		files
 		{
+			"src/x11_platform.h",
+			"src/xkb_unicode.h",
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
+			"src/glx_context.c",
+			"src/posix_time.h",
+			"src/posix_thread.h",
+			"src/posix_module.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
+			"src/posix_poll.h",
+			"src/posix_poll.c",
 			"src/linux_joystick.c"
 		}
 
@@ -102,7 +111,9 @@ project "GLFW"
 		}
 
 	filter "configurations:Debug"
+	runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
+	runtime "Release"
 		optimize "on"
